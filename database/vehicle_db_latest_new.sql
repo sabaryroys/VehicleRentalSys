e5d4fe76c6458f29f0a937e934b4370203d356b9
Sabari Roy <sabarysajithroy@gmail.com>
2022-06-18 15:21:30 +0530

    18/06/2022 :- Added auto maximize of all windows. Added automatic updation of availability status to 'N' for insurance expired vehicles. Fixed bug for insurance expired vehicles being available to rent out to customer . Added rent amount calculation on rental form .

--- database/vehicle_db_latest_new.sql
+++ database/vehicle_db_latest_new.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 06, 2022 at 10:29 AM
+-- Generation Time: Jun 18, 2022 at 09:45 AM
 -- Server version: 8.0.27
 -- PHP Version: 7.4.26
 
@@ -95,10 +95,13 @@ CREATE TABLE IF NOT EXISTS `vehicle_details` (
 --
 
 INSERT INTO `vehicle_details` (`v_id`, `v_reg_no`, `v_type`, `v_model`, `v_avble`, `v_ins_st_dt`, `v_ins_end_dt`, `v_remark`) VALUES
-('V0001', 'KL16F3899', 'Motor Cycle w/Gear', 'YAMAHA R15', 'Y', '2022-05-22', '2023-05-21', 'Available'),
+('V0001', 'KL16F3899', 'Motor Cycle w/Gear', 'YAMAHA R15 V1', 'Y', '2022-05-22', '2023-05-21', 'Available'),
 ('V0002', 'KL16P583', 'Motor Cycle w/Gear', 'ROYAL ENFILED CLASSIC 350', 'Y', '2021-10-18', '2022-10-17', 'Available'),
 ('V0003', 'KL16E8870', 'Motor Cycle w/Gear', 'HERO HONDA PASSION', 'Y', '2021-11-24', '2023-11-23', 'Available'),
-('V0004', 'KL16J2345', 'Scooter', 'HONDA DIO', 'N', '2023-10-18', '2024-10-17', 'Rented');
+('V0004', 'KL16J2345', 'Scooter', 'HONDA DIO', 'N', '2023-10-18', '2024-10-17', 'Rented'),
+('V0005', 'KL16Q6999', 'Car', 'SWIFT DEZIRE', 'Y', '2021-07-09', '2022-07-08', 'Available'),
+('V0006', 'KL23G6573', 'Motor Cycle w/Gear', 'YAMAHA FZ', 'Y', '2022-06-18', '2023-06-19', 'Available'),
+('V0007', 'KL20AE2230', 'Scooter', 'HONDA ACTIVA', 'Y', '2021-06-22', '2022-06-21', 'Available');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
